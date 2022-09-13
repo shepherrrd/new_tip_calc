f65872d14c928ae08ea06bec7fe17af6ebd712f7
shepherrrd <shefkid2@gmail.com>
2022-09-13 13:29:06 +0100

    thiir

---
+++ lib/ui/pallete.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class FvColors {
+  static const Color screen1Background = Color(0xfff5f5f5);
+  static const Color scrollcontainer6Background = Color(0x00000000);
+  static const Color imageview32Background = Color(0xffffffff);
+  static const Color container12Background = Color(0x4cffffff);
+  static const Color container16Background = Color(0x66ffffff);
+  static const Color textview31FontColor = Color(0xff333333);
+  static const Color textview30FontColor = Color(0xff848484);
+  static const Color textview39FontColor = Color(0xff000000);
+}
